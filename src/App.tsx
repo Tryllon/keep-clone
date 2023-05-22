558504587c39ea4ffb2b20ed2f7635e537fdaa16
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-05-22 19:36:21 +0100

    added theme to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import './App.css'
 import NotesApp from './NotesApp'
 import AppContextProvider from './contexts/AppContext'
+import { ThemeProvider } from '@mui/material'
+import theme from './theme'
 
 function App (): React.ReactElement {
   return (
     <div>
-      <AppContextProvider>
-        <NotesApp/>
-      </AppContextProvider>
+      <ThemeProvider
+        theme={theme}
+      >
+        <AppContextProvider>
+          <NotesApp/>
+        </AppContextProvider>
+      </ThemeProvider>
     </div>
   )
 }
