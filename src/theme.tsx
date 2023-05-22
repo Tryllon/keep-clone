ee6d18adde8ff324429e5bbcb731c2f4b968fc2e
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-05-22 19:35:46 +0100

    create MUI theme

---
+++ src/theme.tsx
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffffff'
+    },
+    secondary: {
+      main: '#ffffff'
+    }
+  },
+  typography: {
+    h1: {
+      fontSize: '4.5rem'
+    },
+    subtitle1: {
+    },
+    caption: {
+    },
+    body1: {
+    },
+    body2: {
+    }
+  }
+})
+
+export default theme
